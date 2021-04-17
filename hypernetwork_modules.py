1ae650caa0a3a6398326e9ec2848a9eaab813a08
Grebenkova Olga <olawed@gmail.com>
2021-04-17 19:00:12 +0300

    CIFAR with nans

--- hypernetwork_modules.py
+++ hypernetwork_modules.py
@@ -3,118 +3,27 @@ import torch.nn as nn
 from torch.nn.parameter import Parameter
 
 
-class VarHyperNetwork(nn.Module):
+class HyperNetwork(nn.Module):
 
-    def __init__(self, f_size = 3, z_dim = 64, out_size=16, in_size=16, prior_sigma = 1.0, init_log_sigma=-3.0):
+    def __init__(self, f_size = 3, z_dim = 64, out_size=16, in_size=16):
         super(HyperNetwork, self).__init__()
         self.z_dim = z_dim
         self.f_size = f_size
         self.out_size = out_size
         self.in_size = in_size
-        self.prior_sigma = prior_sigma
 
-        '''self.w1 = Parameter(torch.fmod(torch.randn((self.z_dim, self.out_size*self.f_size*self.f_size)).cuda(),2))
+        self.w1 = Parameter(torch.fmod(torch.randn((self.z_dim, self.out_size*self.f_size*self.f_size)).cuda(),2))
         self.b1 = Parameter(torch.fmod(torch.randn((self.out_size*self.f_size*self.f_size)).cuda(),2))
 
         self.w2 = Parameter(torch.fmod(torch.randn((self.z_dim, self.in_size*self.z_dim)).cuda(),2))
-        self.b2 = Parameter(torch.fmod(torch.randn((self.in_size*self.z_dim)).cuda(),2))'''
-        
-                 
-        self.mean = LinearApprNet((self.z_dim, self.out_size*self.f_size*self.f_size)) # параметры средних            
-        self.log_sigma = LinearApprNet((self.z_dim, self.out_size*self.f_size*self.f_size))) # логарифм дисперсии
-        self.mean_b = LinearApprNet( self.out_size*self.f_size*self.f_size) # то же самое для свободного коэффициента
-        self.log_sigma_b = LinearApprNet( self.out_size*self.f_size*self.f_size)
-        
-        self.mean2 = LinearApprNet((self.z_dim, self.in_size*self.z_dim)) # параметры средних            
-        self.log_sigma2 = LinearApprNet((self.z_dim, self.in_size*self.z_dim)) # логарифм дисперсии
-        self.mean_b2 = LinearApprNet(self.in_size*self.z_dim) # то же самое для свободного коэффициента
-        self.log_sigma_b2 = LinearApprNet( self.in_size*self.z_dim)
-     
-        self.log_sigma.const.data*= 0 # забьем константу нужными нам значениями
-        self.log_sigma.const.data+= init_log_sigma
-     
-        self.log_sigma_b.const.data*= 0 # забьем константу нужными нам значениями
-        self.log_sigma_b.const.data+= init_log_sigma    
-        
-        self.log_sigma2.const.data*= 0 # забьем константу нужными нам значениями
-        self.log_sigma2.const.data+= init_log_sigma
-     
-        self.log_sigma_b2.const.data*= 0 # забьем константу нужными нам значениями
-        self.log_sigma_b2.const.data+= init_log_sigma    
-        
-       
-        
+        self.b2 = Parameter(torch.fmod(torch.randn((self.in_size*self.z_dim)).cuda(),2))
 
-    def forward(self, z, l):
-        
-         if self.training: # во время обучения - сэмплируем из нормального распределения
-            self.eps_w = t.distributions.Normal(self.mean(l), t.exp(self.log_sigma(l)))
-            self.eps_b = t.distributions.Normal(self.mean_b(l), t.exp(self.log_sigma_b(l)))
-            self.eps_w2 = t.distributions.Normal(self.mean(l), t.exp(self.log_sigma(l)))
-            self.eps_b2 = t.distributions.Normal(self.mean_b(l), t.exp(self.log_sigma_b(l)))
-        
-            w = self.eps_w.rsample()
-            b = self.eps_b.rsample()
-            w2 = self.eps_w2.rsample()
-            b2 = self.eps_b2.rsample()
-            
-        else:  # во время контроля - смотрим средние значения параметра        
-            w = self.mean(l) 
-            b = self.mean_b(l)
-            w2 = self.mean(l) 
-            b2 = self.mean_b(l)
+    def forward(self, z, lam):
 
-        h_in = torch.matmul(z, w2) + b2
+        h_in = torch.matmul(z, self.w2) + self.b2
         h_in = h_in.view(self.in_size, self.z_dim)
 
-        h_final = torch.matmul(h_in, w1) + b1
+        h_final = lam * torch.matmul(h_in, self.w1) + self.b1
         kernel = h_final.view(self.out_size, self.in_size, self.f_size, self.f_size)
 
         return kernel
-    
-    def KLD(self, l):        
-        # подсчет дивергенции
-        size = self.in_size, self.out_size
-        out = self.out_size
-        device = self.mean.const.device
-        self.eps_w = t.distributions.Normal(self.mean(l), t.exp(self.log_sigma(l)))
-        self.eps_b = t.distributions.Normal(self.mean_b(l),  t.exp(self.log_sigma_b(l)))
-        self.h_w = t.distributions.Normal(t.zeros(size, device=device), t.ones(size, device=device)*self.prior_sigma*l)
-        self.h_b = t.distributions.Normal(t.zeros(out, device=device), t.ones(out, device=device)*self.prior_sigma*l)
-        self.eps_w2 = t.distributions.Normal(self.mean2(l), t.exp(self.log_sigma2(l)))
-        self.eps_b2 = t.distributions.Normal(self.mean_b2(l),  t.exp(self.log_sigma_b2(l)))
-        k1 = t.distributions.kl_divergence(self.eps_w,self.h_w).sum()        
-        k2 = t.distributions.kl_divergence(self.eps_b,self.h_b).sum()   
-        k3 = t.distributions.kl_divergence(self.eps_w2,self.h_w).sum()        
-        k4 = t.distributions.kl_divergence(self.eps_b2,self.h_b).sum()   
-        return k1+k2+k3+k4
-
-class LinearApprNet(nn.Module):
-    def __init__(self, size,  init_const = 1.0, init_const2 = 1.0):    
-        nn.Module.__init__(self)        
-        if isinstance(size, tuple) and len(size) == 2:
-            self.in_, self.out_ = size
-            self.diagonal = False
-        else:
-            self.out_ = size
-            self.diagonal = True
-                           
-        
-        if self.diagonal:
-            # независимая от параметра lambda часть
-            self.const = nn.Parameter(t.randn(self.out_) * init_const) 
-            self.const2 = nn.Parameter(t.ones(self.out_) * init_const2) 
-            
-            
-        else:
-            self.const = nn.Parameter(t.randn(self.in_, self.out_)) 
-            t.nn.init.xavier_uniform(self.const,  init_const)
-            self.const2 = nn.Parameter(t.randn(self.in_, self.out_)) 
-            t.nn.init.xavier_uniform(self.const2,  init_const2)
-            
-            
-    def forward(self, lam):        
-        if self.diagonal:
-            return self.const + self.const2 * lam
-        else:
-            return self.const + self.const2 * lam 
---
+++ primary_net.py
@@ -0,0 +1,124 @@
+import torch as t
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.parameter import Parameter
+
+from hypernetwork_modules import HyperNetwork
+from resnet_blocks import ResNetBlock
+
+
+class Embedding(nn.Module):
+
+    def __init__(self, z_num, z_dim, device = 'cpu', mode = 'mean', init_log_sigma = -5.0):
+        super(Embedding, self).__init__()
+
+        self.z_list = nn.ParameterList()
+        self.z_num = z_num
+        self.z_dim = z_dim
+        self.init_log_sigma = init_log_sigma
+
+        h,k = self.z_num
+
+        if mode == 'mean':
+            for i in range(h):
+                for j in range(k):
+                    self.z_list.append(Parameter(t.fmod(t.randn(self.z_dim).to(device), 4)))
+        else:
+            for i in range(h):
+                for j in range(k):
+                    self.z_list.append(Parameter(t.ones(self.z_dim).to(device)*self.init_log_sigma))
+
+    def forward(self, hyper_net, lam = None):
+        ww = []
+        h, k = self.z_num
+        for i in range(h):
+            w = []
+            for j in range(k):
+                w.append(hyper_net(self.z_list[i*k + j], lam))
+            ww.append(t.cat(w, dim=1))
+        return t.cat(ww, dim=0)
+           
+    
+
+
+class PrimaryNetwork(nn.Module):
+
+    def __init__(self, z_dim=64, device = 'cpu', prior_sigma = 1.0):
+        super(PrimaryNetwork, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
+        self.bn1 = nn.BatchNorm2d(16)
+
+        self.z_dim = z_dim
+        self.hope1 = HyperNetwork(z_dim=self.z_dim)
+        self.hope2 = HyperNetwork(z_dim=self.z_dim)
+        self.prior_sigma = prior_sigma
+        self.device = device
+
+        self.zs_size = [[1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1],
+                        [2, 1], [2, 2], [2, 2], [2, 2], [2, 2], [2, 2], [2, 2], [2, 2], [2, 2], [2, 2], [2, 2], [2, 2],
+                        [4, 2], [4, 4], [4, 4], [4, 4], [4, 4], [4, 4], [4, 4], [4, 4], [4, 4], [4, 4], [4, 4], [4, 4]]
+
+        self.filter_size = [[16,16], [16,16], [16,16], [16,16], [16,16], [16,16], [16,32], [32,32], [32,32], [32,32],
+                            [32,32], [32,32], [32,64], [64,64], [64,64], [64,64], [64,64], [64,64]]
+
+        self.res_net = nn.ModuleList()
+
+        for i in range(18):
+            down_sample = False
+            if i > 5 and i % 6 == 0:
+                down_sample = True
+            self.res_net.append(ResNetBlock(self.filter_size[i][0], self.filter_size[i][1], downsample=down_sample))
+
+        self.zs_mean = nn.ModuleList()
+        self.zs_sigma = nn.ModuleList()
+
+        for i in range(36):
+            self.zs_mean.append(Embedding(self.zs_size[i], self.z_dim, self.device, 'mean'))
+            self.zs_sigma.append(Embedding(self.zs_size[i], self.z_dim, self.device, 'sigma'))
+
+        self.global_avg = nn.AvgPool2d(8)
+        self.final = nn.Linear(64,10)
+
+    def forward(self, x, lam = None):
+
+        x = F.relu(self.bn1(self.conv1(x)))
+        for i in range(18):
+            # if i != 15 and i != 17:
+            w1_mean = self.zs_mean[2*i](self.hope1, lam)
+            w2_mean = self.zs_mean[2*i+1](self.hope1, lam)
+            w1_sigma = self.zs_sigma[2*i](self.hope2, lam)
+            w2_sigma = self.zs_sigma[2*i+1](self.hope2, lam)
+            if self.training:
+                self.w1_eps = t.distributions.Normal(w1_mean, t.exp(w1_sigma))
+                self.w2_eps = t.distributions.Normal(w2_mean, t.exp(w2_sigma))
+                w1 = self.w1_eps.rsample()
+                w2 = self.w2_eps.rsample()
+            else:
+                w1 = w1_mean
+                w2 = w2_mean
+            x = self.res_net[i](x, w1, w2)
+
+        x = self.global_avg(x)
+        x = self.final(x.view(-1,64))
+
+        return x
+    
+    def KLD(self, l):
+        # подсчет дивергенции
+        device = self.device
+        k = 0
+        for i in range(18):
+            # if i != 15 and i != 17:
+            w1_mean = self.zs_mean[2*i](self.hope1, l)
+            w2_mean = self.zs_mean[2*i+1](self.hope1, l)
+            w1_sigma = self.zs_sigma[2*i](self.hope2, l)
+            w2_sigma = self.zs_sigma[2*i+1](self.hope2, l)
+            self.h_w1 = t.distributions.Normal(t.zeros_like(w1_mean, device=device),
+                                               t.ones_like(w1_sigma, device=device)*self.prior_sigma)
+            self.h_w2 = t.distributions.Normal(t.zeros_like(w2_mean, device=device),
+                                               t.ones_like(w2_sigma, device=device)*self.prior_sigma)
+            self.w1_eps = t.distributions.Normal(w1_mean, w1_sigma)
+            self.w2_eps = t.distributions.Normal(w2_mean, w2_sigma)
+            k += t.distributions.kl_divergence(self.w1_eps, self.h_w1).sum()
+            k += t.distributions.kl_divergence(self.w2_eps, self.h_w2).sum()
+        return k
